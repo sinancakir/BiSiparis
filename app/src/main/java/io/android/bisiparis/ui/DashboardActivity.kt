daab9704cbd9c231d9122eec8bbb311f78b8ac5a
Sinan Çakır <sinanckrr@gmail.com>
2018-02-21 17:58:22 +0300

    Update DashboardActivity.kt

--- app/src/main/java/io/android/bisiparis/ui/DashboardActivity.kt
+++ app/src/main/java/io/android/bisiparis/ui/DashboardActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AlertDialog
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
@@ -14,16 +15,18 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import io.android.bisiparis.R
 import io.android.bisiparis.adapter.OrderListAdapter
 import io.android.bisiparis.enums.GeneralInfo
 import io.android.bisiparis.helper.DatabaseHelper
+import io.android.bisiparis.interfaces.CustomClickListener
 import io.android.bisiparis.model.OrderInfo
 import io.android.bisiparis.singleton.DataHolder
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
-class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefreshLayout.OnRefreshListener {
+class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefreshLayout.OnRefreshListener, CustomClickListener {
 
     private val rcyOrder by lazy { findViewById<RecyclerView>(R.id.activity_dashboard_rcyOrders) }
     private val swipeRefreshLayout by lazy { findViewById<SwipeRefreshLayout>(R.id.activity_dashboard_swipe) }
@@ -31,6 +34,8 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefres
     private val addDialog by lazy { AlertDialog.Builder(this).create() }
     private val btnTakeOrder by lazy { addDialog.findViewById<Button>(R.id.custom_alert_add_order_name_btnTakeOrder) }
     private val edtCustomerName by lazy { addDialog.findViewById<EditText>(R.id.custom_alert_add_order_name_edtCustomerName) }
+    //private val btnUpdateOrder by lazy { addDialog.findViewById<Button>(R.id.custom_alert_update_order_name_btnTakeOrder) }
+    //private val edtUpdateName by lazy { addDialog.findViewById<EditText>(R.id.custom_alert_update_order_name_edtCustomerName) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,15 +47,15 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefres
     }
 
     private fun initEvent() {
-        val result : ArrayList<OrderInfo> = databaseHelper.getAllOrders()
+        val result: ArrayList<OrderInfo> = databaseHelper.getAllOrders()
         swipeRefreshLayout.setOnRefreshListener(this)
-        /*if (result.size == 0) {
+        if (result.size == 0) {
             activity_dashboard_txtNoOrder.visibility = View.VISIBLE
             activity_dashboard_txtNoOrder.text = GeneralInfo.NoOrderValues.toString()
             swipeRefreshLayout.isRefreshing = false
-        }*/
+        }
         val linearLayoutManager = LinearLayoutManager(this)
-        val orderListAdapter = OrderListAdapter(result)
+        val orderListAdapter = OrderListAdapter(result, this)
 
         val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0,
                 ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
@@ -70,11 +75,13 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefres
         val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
 
         rcyOrder.layoutManager = linearLayoutManager
+        val divider = DividerItemDecoration(rcyOrder.context, linearLayoutManager.orientation)
+        rcyOrder.addItemDecoration(divider)
         rcyOrder.adapter = orderListAdapter
         itemTouchHelper.attachToRecyclerView(rcyOrder)
     }
 
-    private fun createAlertDialog() {
+    private fun createAddAlertDialog() {
         val layoutInflater = LayoutInflater.from(this)
         val alertView = layoutInflater.inflate(R.layout.custom_alert_add_order_name, null, false)
 
@@ -84,27 +91,42 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefres
         btnTakeOrder?.setOnClickListener(this)
     }
 
+    /*private fun createUpdateAlertDialog(orderKey: Long = 0L) {
+        val layoutInflater = LayoutInflater.from(this)
+        val alertView = layoutInflater.inflate(R.layout.custom_alert_update_order_name, null, false)
+
+        addDialog.setView(alertView)
+        addDialog.show()
+        btnUpdateOrder?.tag = orderKey
+        btnUpdateOrder?.setOnClickListener(this)
+    }*/
+
+    /*private fun eventUpdate(button: Button) {
+        val orderInfo = OrderInfo(button.tag.toString().toLong(), "", "", "", "", "")
+        databaseHelper.updateName(orderInfo)
+    }*/
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_activity_dashboard, menu)
         return true
     }
 
     override fun onRefresh() {
-        val result : ArrayList<OrderInfo> = databaseHelper.getAllOrders()
-        /*if (result.size == 0) {
+        val result: ArrayList<OrderInfo> = databaseHelper.getAllOrders()
+        if (result.size == 0) {
             activity_dashboard_txtNoOrder.visibility = View.VISIBLE
             activity_dashboard_txtNoOrder.text = GeneralInfo.NoOrderValues.toString()
             swipeRefreshLayout.isRefreshing = false
-        }*/
-        (rcyOrder.adapter as OrderListAdapter).setOrderList(result)
-        swipeRefreshLayout.isRefreshing = false
-        Toast.makeText(applicationContext, "Harikayım ben!", Toast.LENGTH_SHORT).show()
+        } else {
+            (rcyOrder.adapter as OrderListAdapter).setOrderList(result)
+            swipeRefreshLayout.isRefreshing = false
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item!!.itemId) {
             R.id.menu_activity_dashboard_add_order -> {
-                createAlertDialog()
+                createAddAlertDialog()
             }
         }
         return super.onOptionsItemSelected(item)
@@ -113,15 +135,28 @@ class DashboardActivity : AppCompatActivity(), View.OnClickListener, SwipeRefres
     override fun onClick(view: View?) {
         when ((view as Button).id) {
             R.id.custom_alert_add_order_name_btnTakeOrder -> {
-                addDialog.dismiss()
                 DataHolder.userName = edtCustomerName?.text.toString()
                 edtCustomerName?.text = null
                 val intent = Intent(this, ChoiceActivity::class.java)
                 intent.putExtra(GeneralInfo.CustomerName.toString(), DataHolder.userName)
                 startActivity(intent)
+                addDialog.dismiss()
             }
+            /*R.id.custom_alert_update_order_name_btnTakeOrder -> {
+                eventUpdate(view)
+                addDialog.dismiss()
+            }*/
         }
     }
 
+    override fun onResume() {
+        (rcyOrder.adapter as OrderListAdapter).setOrderList(databaseHelper.getAllOrders())
+        swipeRefreshLayout.isRefreshing = false
+        super.onResume()
+    }
 
+    override fun onClickListener(orderInfo: OrderInfo, position: Int) {
+        Toast.makeText(applicationContext, "${orderInfo.orderName}", Toast.LENGTH_SHORT).show()
+        //createUpdateAlertDialog(orderInfo.orderId)
+    }
 }
